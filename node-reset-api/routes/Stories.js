81137896bd782ec15876fd48b8bec177020b3f59
Tsega Tigneh <136186978+segish@users.noreply.github.com>
2023-06-15 02:01:17 +0300

    Update Stories.js

--- node-reset-api/routes/Stories.js
+++ node-reset-api/routes/Stories.js
@@ -6,15 +6,9 @@ const jwt = require("jsonwebtoken");
 
 //create stories
 router.post("/", async (req, res) => {
-    const token = req.cookies.accessToken;
-    if(!token) return res.status(401).json("You must login first!");
-
-    jwt.verify(token, "secretkey", async (err, userInfo)=>{
-        if(err) return res.status(403).json("Token is not valid!");
-        
-        const currentUser = await User.findById(userInfo.id);
+        const currentUser = await User.findById(req.query.user);
         const values={
-                userId: userInfo.id,
+                userId: req.query.user,
                 img : req.body.img,
                 name : currentUser.name,
             }
@@ -26,30 +20,23 @@ router.post("/", async (req, res) => {
         } catch (err) {
         res.status(500).json(err);
         } 
-    })
   });
 //get timeline stories
 
 router.get("/", async (req, res) => {
-    const token = req.cookies.accessToken;
-    if(!token) return res.status(401).json("You must login first!");
-
-    jwt.verify(token, "secretkey", async (err, userInfo)=>{
-        if(err) return res.status(403).json("Token is not valid!");
-    try {
-      const currentUser = await User.findById(userInfo.id);
-      const userStories = await Story.find({ userId: currentUser._id });
-      const friendStories = await Promise.all(
-        currentUser.followings.map((friendId) => {
-          return Story.find({ userId: friendId });
-        })
-      );
-      res.json(userStories.concat(...friendStories))
-    } catch (err) {
-      res.status(500).json(err);
-    }
-})
-  });
+  try {
+    const currentUser = await User.findById(req.query.user);
+    const userStories = await Story.find({ userId: currentUser._id });
+    const friendStories = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Story.find({ userId: friendId });
+      })
+    );
+    res.json(userStories.concat(...friendStories))
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
   
 
   module.exports = router;
