e58ab654ab96e5764d48e6dba74ddb3247d29140
Tsega Tigneh <136186978+segish@users.noreply.github.com>
2023-06-14 19:28:17 +0300

    Update index.js

--- node-reset-api/index.js
+++ node-reset-api/index.js
@@ -27,7 +27,7 @@ app.use((req,res,next) => {
 app.use(express.json());
 app.use(
     cors({
-        origin: "https://6489e9caf51c400d329d3413--coruscating-kangaroo-3efce4.netlify.app/",
+        origin: "https://6489e9caf51c400d329d3413--coruscating-kangaroo-3efce4.netlify.app",
     })
 );
 app.use(cookieParser());
