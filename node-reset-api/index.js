c2336ab244199a30c0eda957add076af99a4741a
Tsega Tigneh <136186978+segish@users.noreply.github.com>
2023-07-01 22:59:10 +0300

    Update index.js

--- node-reset-api/index.js
+++ node-reset-api/index.js
@@ -24,7 +24,8 @@ app.use((req,res,next) => {
     res.header("Access-Control-Allow-Credentials", true);
     next();
 });
-app.use(express.json());
+app.use(express.json({limit : '50mb'}));
+app.use(express.urlencoded({limit : '50mb',extended : true}));
 app.use(
     cors({
         origin: "https://64a079c4de745159447bd8a2--shiny-otter-825bc0.netlify.app",
